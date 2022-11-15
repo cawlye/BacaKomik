c5e59e67d6c2eb21ad92a0b040f2e6e6c52caae8
cawlye <113624168+cawlye@users.noreply.github.com>
2022-11-15 12:25:18 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,10 +2,10 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <div className="p-3 flex justify-between items-center bg-green-500 text-white">
+    <div className="p-3 flex justify-between items-center bg-sky-500 text-white">
       <div className="">
         <Link href="/">
-          <a className="text-xl font-bold">BacaKomik</a>
+          <a className="text-xl font-bold text-black-500 uppercase text-center my-3">Cawlye Komik</a>
         </Link>
       </div>
       <div className=""></div>
